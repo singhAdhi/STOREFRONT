c039454e96e40ea7662c661595219cb047d18e36
ManishAwhade <manish.awhade>
2023-12-14 13:15:32 +0530

    redemption impl

--- src/views/home/redemption-menu/RedemptionMenu.jsx
+++ src/views/home/redemption-menu/RedemptionMenu.jsx
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./redemptionmenu.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const RedemptionMenu = () => {
+  const navigate = useNavigate();
+  const [redemptions, setredemptions] = useState(null);
+
+  useEffect(() => {
+    import("../../../dummy/SearchCategories_DATA")
+      .then((module) => {
+        setredemptions(
+          module.SearchCategories_DATA.filter((x) => x.ParentId == null).sort(
+            (a, b) => a - b
+          )
+        );
+      })
+      .catch((error) => {
+        console.error("Error loading data:", error);
+      });
+  }, []);
+
   return (
-    <>
-      <Link to="/FlightSearch">Flight</Link>
-      <Link to="/HotelSearch">Hotel</Link>
-      <Link to="/Shop">Shop</Link>
-    </>
+    <div className="dvRedemptions pt-5">
+      {redemptions &&
+        redemptions.map((item, idx) => (
+          <div
+            key={idx}
+            className="redemption-card"
+            onClick={() => navigate(item.Name.toLowerCase())}
+          >
+            {item.PrimaryImage && item.PrimaryImage.Url && (
+              <div className="imageBox">
+                <img src={item.PrimaryImage.Url} alt={item.Name} />
+              </div>
+            )}
+            <p style={{ paddingTop: 10 }}>{item.Name}</p>
+          </div>
+        ))}
+    </div>
   );
 };
 
