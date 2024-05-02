27a18c1e146376696aa3b4f1e3e23c1d00cfadde
Adhiraj Singh <adhirajsingh7303@gmail.com>
2024-05-02 18:54:05 +0530

    hotel

--- src/api/services.js
+++ src/api/services.js
@@ -1,7 +1,7 @@
 import createAxiosInstance from "./axios";
 import { ENDPOINTS_BASE_URL } from "../config";
 
-let url = `http://localhost:5173`;
+const url = import.meta.env.REACT_APP_API_URL || "http://localhost:5173";
 const shopApi = createAxiosInstance(url); //(ENDPOINTS_BASE_URL.shop);
 const hotelApi = createAxiosInstance(url); //(ENDPOINTS_BASE_URL.hotel);
 const flightApi = createAxiosInstance(url); //(ENDPOINTS_BASE_URL.flight);
